4abd5f19e1b5aa46a5f59c50ee7a25df480fe280
Andrey Fedorov <87073832+AndreyFedorov78@users.noreply.github.com>
2022-03-27 17:37:44 +0300

    добавлена функция очитки всей базы

--- scada/views.py
+++ scada/views.py
@@ -9,10 +9,13 @@ from .models import Sensor
 from .serialalizers import SensorSerializer, SensorDetailSerializer, SensorDataOnlySerializer
 from .blynk import Blynk
 from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class Index(View):
     @staticmethod
     def get(request):
+        # Sensor.objects.all().delete()
         return render(request, 'scada/index.html')
 
 
@@ -29,20 +32,32 @@ class Vent(APIView):
         return Response(status=201)
 
 
+##class SensorView(LoginRequiredMixin,APIView):
 class SensorView(APIView):
     @staticmethod
     def get(request):
-        sensor = Sensor.objects.all()
+        sensor = Sensor.objects.all()[:200]
         sv_serializer = SensorSerializer(sensor, many=True)
         return Response(sv_serializer.data)
 
     @staticmethod
-    def post(request):
+    def post(request):  # получение данных от датчика и записб в бд
         sensor = SensorDetailSerializer(data=request.data)
         if sensor.is_valid():
-            sensor.save()
+            sensor = sensor.save()
+            sensor = Sensor.objects.filter(sensorId=sensor.sensorId).filter(type=sensor.type).order_by('-date')[:3]
+            if len(sensor) == 3:
+                if sensor[0].date - sensor[2].date < datetime.timedelta(minutes=15):
+                    print('hello', sensor[1],' id', sensor[1].id)
+                    sensor[1].delete()
+
         return Response(status=201)
 
+
+    @staticmethod
+    def delete(request):  # получение данных от датчика и записб в бд
+        Sensor.objects.all().delete()
+        return Response(status=201)
 
 class SensorDetailView(APIView):
     @staticmethod
@@ -69,16 +84,16 @@ class AllSensors(APIView):
 class SensorLastDays(APIView):
     @staticmethod
     def get(request, sensor_id, data_type, days):
-        start_date = timezone.now()-datetime.timedelta(days=days)
+        start_date = timezone.now() - datetime.timedelta(days=days)
         sensor = Sensor.objects.all().order_by('date').filter(sensorId=sensor_id, type=data_type, date__gt=start_date)
         sensor_new = []
-        actual_date = timezone.now()-datetime.timedelta(days=days)
+        actual_date = timezone.now() - datetime.timedelta(days=days)
         dat = 0
         counter = 0
         for i in sensor:
-            if not(i.date.date() == actual_date.date() and  i.date.hour == actual_date.hour):
+            if not (i.date.date() == actual_date.date() and i.date.hour == actual_date.hour):
                 if counter != 0:
-                    sensor_new.append({'date' : actual_date, 'data': dat / counter})
+                    sensor_new.append({'date': actual_date, 'data': dat / counter})
                 actual_date = i.date
                 counter = 0
                 dat = 0
@@ -87,3 +102,4 @@ class SensorLastDays(APIView):
             counter += 1
         sensor_new.append({'date': actual_date, 'data': dat / counter if counter != 0 else 0})
         return Response(sensor_new)
+
